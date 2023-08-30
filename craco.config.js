ef8cd5490988c2ff51685ff9813f352815c6c632
Pawan Paudel <pawanpaudel93@gmail.com>
2023-08-30 21:19:11 +0545

    Replace rewired with craco

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+        ({ constructor }) =>
+          constructor && constructor.name === "ModuleScopePlugin"
+      );
+
+      webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+      return webpackConfig;
+    },
+    plugins: {
+      add: [new NodePolyfillPlugin()],
+    },
+  },
+};
